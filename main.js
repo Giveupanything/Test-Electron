b4d8d87c60a4128dc4d9304cbcb1ce0eba8deb5c
GiveUpAnything <13541267+giveupanything@user.noreply.gitee.com>
2025-03-11 18:28:34 +0800

    init-proj

---
+++ main.js
@@ -0,0 +1,19 @@
+const { BrowserWindow, app } = require('electron');
+
+const winInstance = new BrowserWindow({
+  width: 500,
+  height: 500,
+});
+
+winInstance.loadURL('https://www.baidu.com');
+
+const contents = winInstance.webContents;
+
+console.log(contents, 'contents --->');
+
+app.on('window-all-closed', () => {
+  // win32、darwin、linux
+  if(process.platform !== 'darwin') {
+    app.quit();
+  }
+})
